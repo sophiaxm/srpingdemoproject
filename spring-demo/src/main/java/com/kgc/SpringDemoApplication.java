6494bb605465be1fedd334d1b85250a11694a972
xm <shophia@126.com>
2021-10-13 16:20:17 +0800

    第一次修改

--- spring-demo/src/main/java/com/kgc/SpringDemoApplication.java
+++ spring-demo/src/main/java/com/kgc/SpringDemoApplication.java
@@ -9,9 +9,6 @@ import org.springframework.context.annotation.PropertySource;
 public class SpringDemoApplication {
     public static void main(String[] args) {
 	//启动操作
-	// 憋人修改的代码
-        //组员a修改master
-        //组员b的修改
         SpringApplication.run(SpringDemoApplication.class, args);
     }
 
--- spring-demo/src/main/java/com/kgc/controller/HelloController.java
+++ spring-demo/src/main/java/com/kgc/controller/HelloController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+//控制器
 @RestController
 public class HelloController {
 
